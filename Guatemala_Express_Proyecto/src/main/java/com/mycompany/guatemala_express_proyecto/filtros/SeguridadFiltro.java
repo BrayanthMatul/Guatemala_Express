f96e5eb0905c7b10201bdfd414c3be450acb4cd4
matul <brayanth.matul146@gmail.com>
2026-09-06 12:15:07 -0600

    filtro para asegurar las rutas

---
+++ Guatemala_Express_Proyecto/src/main/java/com/mycompany/guatemala_express_proyecto/filtros/SeguridadFiltro.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ */
+package com.mycompany.guatemala_express_proyecto.filtros;
+
+import java.io.IOException;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author matul
+ */
+@WebFilter(filterName = "SeguridadFiltro", urlPatterns = { "/cliente/*", "/chofer/*", "/administrador_sucursal/*",
+        "/administrador_sistema/*" }, dispatcherTypes = { DispatcherType.REQUEST })
+public class SeguridadFiltro implements Filter {
+
+    public SeguridadFiltro() {
+    }
+
+    /**
+     *
+     * @param request  The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain    The filter chain we are processing
+     *
+     * @exception IOException      if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        if (session == null || session.getAttribute("usuarioId") == null) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
