62a91474b0ea0d7481d20214d84b190a75165449
matul <brayanth.matul146@gmail.com>
2026-09-06 12:14:43 -0600

    Excepcciones para el inicio de sesion

---
+++ Guatemala_Express_Proyecto/src/main/java/com/mycompany/guatemala_express_proyecto/exceptions/DatosIncompletosException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.guatemala_express_proyecto.exceptions;
+
+/**
+ *
+ * @author matul
+ */
+public class DatosIncompletosException extends Exception {
+
+    public DatosIncompletosException(String message) {
+        super(message);
+    }
+
+}
---
+++ Guatemala_Express_Proyecto/src/main/java/com/mycompany/guatemala_express_proyecto/exceptions/UsuarioDesactivadoException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.guatemala_express_proyecto.exceptions;
+
+/**
+ *
+ * @author matul
+ */
+public class UsuarioDesactivadoException extends Exception {
+    public UsuarioDesactivadoException(String mensaje) {
+        super(mensaje);
+    }
+
+}
