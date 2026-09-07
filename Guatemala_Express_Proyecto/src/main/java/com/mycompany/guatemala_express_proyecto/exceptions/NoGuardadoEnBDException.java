f65231097ca6970a11a73d1d68f1c55430c4fb05
matul <brayanth.matul146@gmail.com>
2026-09-07 07:03:18 -0600

    Excepcciones para errores al guardar datos

---
+++ Guatemala_Express_Proyecto/src/main/java/com/mycompany/guatemala_express_proyecto/exceptions/NoGuardadoEnBDException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.guatemala_express_proyecto.exceptions;
+
+/**
+ *
+ * @author matul
+ */
+public class NoGuardadoEnBDException extends Exception {
+
+    public NoGuardadoEnBDException(String message) {
+        super(message);
+    }
+
+}
